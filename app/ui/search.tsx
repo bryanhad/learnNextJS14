08c5888b1bd6e31b3e8211333424ab4cb90fc4ee
bryanhad <bryanhadinata@gmail.com>
2023-11-26 21:09:40 +0700

    chapter 11: added search and pagination feature!
    for the search feature we can make a client component because it is ultimately a client side event! we need to get the user's input!
    so to implement this search feature, we need to use some of the hooks next gave us:
    - useSearchParams => to get the query params of current URL as an object.
    - usePathName => to get the current pathname of the url.
    - useRouter => we can access one of the methods that this hook returns, that is replace. we can use it to navigate between routes programmatically! noice.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,26 +1,26 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search({placeholder}:{placeholder:string}) {
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    console.log(term);
-  }
-
-  if (typeof window !== 'undefined') {
-    console.log('You are on the browser');
-    // 👉️ can use alert()
-  } else {
-    console.log('You are on the server');
-    // 👉️ can't use alert()
-  }
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1')
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   return (
-    <>
-      <button onClick={(e) => console.log('kontoidd')}>CLICK ME</button>
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
@@ -29,13 +29,11 @@ export default function Search({placeholder}:{placeholder:string}) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          console.log('kontol');
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
-    </>
-
   );
 }
